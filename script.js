b76a947183e76fb5a51b51967c801448361f762c
suningun <suningun55555@gmail.com>
2026-04-19 10:11:28 +0700

    fix: update image paths to remove leading slashes

--- script.js
+++ script.js
@@ -2,56 +2,56 @@
 let products = [
   {
     id: 1,
-    img: "/src/products/ID1.webp",
+    img: "src/products/ID1.webp",
     name: "Swan Ballet Six-Colour Makeup Palette",
     price: 35,
     category: "Face",
   },
   {
     id: 2,
-    img: "/src/products/ID2.webp",
+    img: "src/products/ID2.webp",
     name: "Swan Ballet Embossed Blush",
     price: 26,
     category: "Face",
   },
   {
     id: 3,
-    img: "/src/products/ID3.webp",
+    img: "src/products/ID3.webp",
     name: "Butterfly Cloud Collar Rouge Box Highlighting & Blush Duo Powder",
     price: 18.2,
     category: "Eyes",
   },
   {
     id: 4,
-    img: "/src/products/ID4.webp",
+    img: "src/products/ID4.webp",
     name: "Strawberry Cupid All Day Glow Liquid Blush",
     price: 22,
     category: "Face",
   },
   {
     id: 5,
-    img: "/src/products/ID5.webp",
+    img: "src/products/ID5.webp",
     name: "Butterfly Cloud Collar Embossed Six-Colour Makeup Palette",
     price: 31.5,
     category: "Face",
   },
   {
     id: 6,
-    img: "/src/products/ID6.webp",
+    img: "src/products/ID6.webp",
     name: "Strawberry Cupid Pressed Powder",
     price: 35,
     category: "Face",
   },
   {
     id: 7,
-    img: "/src/products/ID7.webp",
+    img: "src/products/ID7.webp",
     name: "Little Angel Dewy Hydrating Primer",
     price: 35,
     category: "Face",
   },
   {
     id: 8,
-    img: "/src/products/ID8.webp",
+    img: "src/products/ID8.webp",
     name: "Shell's Jewel 6-Colour Makeup Palette",
     price: 35,
     category: "Face",
@@ -59,42 +59,42 @@ let products = [
 
   {
     id: 9,
-    img: "/src/products/ID9.webp",
+    img: "src/products/ID9.webp",
     name: "Butterfly Cloud Collar Liquid Eyeliner",
     price: 15,
     category: "Eyes",
   },
   {
     id: 10,
-    img: "/src/products/ID10.webp",
+    img: "src/products/ID10.webp",
     name: "Violet Strawberry Rococo Eyeshadow Palette",
     price: 31.5,
     category: "Eyes",
   },
   {
     id: 11,
-    img: "/src/products/ID11.webp",
+    img: "src/products/ID11.webp",
     name: "Bunny Garden Gel Eyeliner",
     price: 15,
     category: "Eyes",
   },
   {
     id: 12,
-    img: "/src/products/ID12.webp",
+    img: "src/products/ID12.webp",
     name: "The Sweetie Bear Dual-Ended Brow Gel & Pencil",
     price: 15,
     category: "Eyes",
   },
   {
     id: 13,
-    img: "/src/products/ID13.webp",
+    img: "src/products/ID13.webp",
     name: "Little Angel 9-Colour Eyeshadow Palette",
     price: 45,
     category: "Eyes",
   },
   {
     id: 14,
-    img: "/src/products/ID14.webp",
+    img: "src/products/ID14.webp",
     name: "Midsummer Fairytales Eyebrow Palette",
     price: 16,
     category: "Eyes",
@@ -102,56 +102,56 @@ let products = [
 
   {
     id: 15,
-    img: "/src/products/ID15.webp",
+    img: "src/products/ID15.webp",
     name: "Bunny Garden Lip Balm",
     price: 15,
     category: "Lips",
   },
   {
     id: 16,
-    img: "/src/products/ID16.webp",
+    img: "src/products/ID16.webp",
     name: "Butterfly Cloud Collar Glossy Lipstick",
     price: 20,
     category: "Lips",
   },
   {
     id: 17,
-    img: "/src/products/ID17.webp",
+    img: "src/products/ID17.webp",
     name: "The Sweetie Bear Coating Lip Jelly",
     price: 20,
     category: "Lips",
   },
   {
     id: 18,
-    img: "/src/products/ID18.webp",
+    img: "src/products/ID18.webp",
     name: "Swan Ballet Shine Lipstick",
     price: 14,
     category: "Lips",
   },
   {
     id: 19,
-    img: "/src/products/ID19.webp",
+    img: "src/products/ID19.webp",
     name: "Shell's Jewel Nourishing Lip Glaze",
     price: 14,
     category: "Lips",
   },
   {
     id: 20,
-    img: "/src/products/ID20.webp",
+    img: "src/products/ID20.webp",
     name: "Little Angel Matte Lipstick",
     price: 20,
     category: "Lips",
   },
   {
     id: 21,
-    img: "/src/products/ID21.webp",
+    img: "src/products/ID21.webp",
     name: "Strawberry Cupid Cake Lip Cream",
     price: 20,
     category: "Lips",
   },
   {
     id: 22,
-    img: "/src/products/ID22.webp",
+    img: "src/products/ID22.webp",
     name: "Strawberry Rococo Cloud Lip Cream",
     price: 16,
     category: "Lips",
@@ -159,84 +159,84 @@ let products = [
 
   {
     id: 23,
-    img: "/src/products/ID23.webp",
+    img: "src/products/ID23.webp",
     name: "Strawberry Rococo Hand Mirror",
     price: 25,
     category: "Accessories & Applicators",
   },
   {
     id: 24,
-    img: "/src/products/ID24.webp",
+    img: "src/products/ID24.webp",
     name: "Midsummer Fairytales Hand Mirror",
     price: 25,
     category: "Accessories & Applicators",
   },
   {
     id: 25,
-    img: "/src/products/ID25.webp",
+    img: "src/products/ID25.webp",
     name: "Butterfly Cloud Collar Hand Mirror",
     price: 25,
     category: "Accessories & Applicators",
   },
   {
     id: 26,
-    img: "/src/products/ID26.webp",
+    img: "src/products/ID26.webp",
     name: "Bunny Garden Hand Mirror",
     price: 25,
     category: "Accessories & Applicators",
   },
   {
     id: 27,
-    img: "/src/products/ID27.webp",
+    img: "src/products/ID27.webp",
     name: "The Sweetie Bear Hand Mirror",
     price: 25,
     category: "Accessories & Applicators",
   },
   {
     id: 28,
-    img: "/src/products/ID28.webp",
+    img: "src/products/ID28.webp",
     name: "Strawberry Cupid Hand Mirror",
     price: 25,
     category: "Accessories & Applicators",
   },
   {
     id: 29,
-    img: "/src/products/ID29.webp",
+    img: "src/products/ID29.webp",
     name: "Strawberry Cupid Scented Hand Cream",
     price: 15,
     category: "Accessories & Applicators",
   },
   {
     id: 30,
-    img: "/src/products/ID30.webp",
+    img: "src/products/ID30.webp",
     name: "Bunny Garden Hair Clip",
     price: 8,
     category: "Accessories & Applicators",
   },
   {
     id: 31,
-    img: "/src/products/ID31.webp",
+    img: "src/products/ID31.webp",
     name: "Bunny Garden Angled Blush Brush",
     price: 15,
     category: "Accessories & Applicators",
   },
   {
     id: 32,
-    img: "/src/products/ID32.webp",
+    img: "src/products/ID32.webp",
     name: "Flower Knows Chocolate Leather Tote Bag",
     price: 45,
     category: "Accessories & Applicators",
   },
   {
     id: 33,
-    img: "/src/products/ID33.webp",
+    img: "src/products/ID33.webp",
     name: "Blush Brush (Random)",
     price: 15,
     category: "Accessories & Applicators",
   },
   {
     id: 34,
-    img: "/src/products/ID34.webp",
+    img: "src/products/ID34.webp",
     name: "Shell's Jewel Hand Mirror",
     price: 25,
     category: "Accessories & Applicators",
@@ -253,7 +253,7 @@ const createHeader = () => {
                 <i class="fas fa-bars"></i>
             </button>
             <div class="logo">
-                <img src="/src/logo.avif" alt="FlowerKnows Logo">
+                <img src="src/logo.avif" alt="FlowerKnows Logo">
             </div>
 
             <nav>
@@ -281,7 +281,7 @@ const createFooter = () => {
 
         <ul class="footer-page">
             <li>
-                <img src="/src/additional-images/live_your_fairytale.webp" alt="live with fary Tale">
+                <img src="src/additional-images/live_your_fairytale.webp" alt="live with fary Tale">
             </li>
         </ul>
 
